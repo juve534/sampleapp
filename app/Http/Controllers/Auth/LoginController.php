54ac863df78a2131b1cd51aa26d7e1e5a9875ce9
kimura-tsuyoshi <maxim_4141.rock@zoho.com>
2018-10-30 02:09:28 +0900

    [add] ログイン・ログアウト作成
    ログインとログアウトの処理を作成

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,9 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Contracts\Auth\StatefulGuard;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -35,5 +37,20 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * ログアウト時に/homeに飛ばしたいため、オーバーライド
+     * @param Request $request
+     * @return \Illuminate\Http\RedirectResponse|\Illuminate\Routing\Redirector
+     * @see StatefulGuard::logout()
+     */
+    public function logout(Request $request)
+    {
+        $this->guard()->logout();
+
+        $request->session()->invalidate();
+
+        return redirect('/home');
     }
 }
---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+    <meta charset="UTF-8">
+</head>
+<body>
+<h1>ログインフォーム</h1>
+@isset($message)
+    <p style="color: red">{{$message}}</p>
+@endisset
+<form name="loginform" action="/auth/login" method="post">
+    {{csrf_field()}}
+    メールアドレス:<input type="text" name="email" size="30" value="{{old('email')}}"><span>{{$errors->first('email')}}</span><br>
+    パスワード:<input type="password" name="password" size="30"><span>{{$errors->first('password')}}</span><br>
+    <button type="submit" name="action" value="send">送信</button>
+</form>
+</form>
+</body>
+</html>
