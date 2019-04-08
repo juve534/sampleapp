ede61fe41a8fb4ce984aeba085aa25a08c9a9028
juve534 <maxim_4141.rock@zoho.com>
2019-04-09 02:05:30 +0900

    :construction: ブログ取得APIのテスト作成
    データ取得時にテストがコケる

---
+++ tests/Feature/PostsTest.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Posts;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class PostsTest extends TestCase
+{
+    const BASE_URL = 'api/v1/posts';
+
+    public function testStatusCode()
+    {
+        $response = $this->get(self::BASE_URL);
+        $response->assertStatus(200);
+        $response->assertHeader('content-type', 'application/json');
+    }
+
+    public function testPostsData()
+    {
+        $posts = factory(Posts::class, 5)->create()->toArray();
+        $response = $this->get(self::BASE_URL);
+        $response->assertStatus(200);
+        $response->assertHeader('content-type', 'application/json');
+        $response->assertExactJson(
+            [
+            'data' => $posts,
+            ]
+        );
+    }
+}
