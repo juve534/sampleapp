729e7ff8c53c448c17f6c26af2e54bfb7d91f05a
juve534 <maxim_4141.rock@zoho.com>
2019-04-15 01:12:29 +0900

    :sparkles: TB:commentsの リソース作成
    APIからcommentsのデータを取得.

---
+++ app/Http/Resources/CommentsResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\Resource;
+
+class CommentsResource extends Resource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    public function toArray($request)
+    {
+        return [
+            'id' => $this->resource['id'],
+            'commenter' => $this->resource['commenter'],
+            'body' => $this->resource['body'],
+        ];
+    }
+}
