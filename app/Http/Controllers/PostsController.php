bb5402e8b59e8d2a4b8a5c277f3d5181832edd69
kimura-tsuyoshi <kimura-tsuyoshi@dmm.com>
2019-08-15 04:38:09 +0900

    :wastebasket: 全件取得時はリソースを使わない
    リソースの学習は単一から始めるため.

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\PostsResource;
 use App\Models\Posts;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class PostsController extends Controller
@@ -11,6 +12,8 @@ class PostsController extends Controller
     public function index()
     {
         $data = Posts::all();
+        return JsonResponse::create($data);
+    }
         return new PostsResource($data);
     }
 }
--- tests/Feature/PostsTest.php
+++ tests/Feature/PostsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Comments;
 use App\Models\Posts;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -12,23 +13,25 @@ class PostsTest extends TestCase
 
     const BASE_URL = 'api/v1/posts';
 
-    public function testStatusCode()
-    {
-        $response = $this->get(self::BASE_URL);
-        $response->assertStatus(200);
-        $response->assertHeader('content-type', 'application/json');
-    }
-
-    public function testPostsData()
+    /**
+     * indexメソッドの正常系
+     * @test
+     */
+    public function testIndex200()
     {
         $posts = factory(Posts::class, 5)->create()->toArray();
         $response = $this->get(self::BASE_URL);
         $response->assertStatus(200);
         $response->assertHeader('content-type', 'application/json');
         $response->assertExactJson(
-            [
-            'data' => $posts,
-            ]
+            $posts
+        );
+    }
+
+    {
+        $response->assertStatus(200);
+        $response->assertHeader('content-type', 'application/json');
+        $response->assertExactJson(
         );
     }
 }
