4464cff45864eaf89050a2e00d783f799085d284
kimura-tsuyoshi <kimura-tsuyoshi@dmm.com>
2019-08-15 01:24:38 +0900

    :recycle: 不要なuseの削除

--- tests/Feature/PostsTest.php
+++ tests/Feature/PostsTest.php
@@ -4,7 +4,6 @@ namespace Tests\Feature;
 
 use App\Models\Posts;
 use Tests\TestCase;
-use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class PostsTest extends TestCase
