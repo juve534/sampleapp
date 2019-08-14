b1b2baead7322bbf7d0118a48aae0a271976faf7
kimura-tsuyoshi <kimura-tsuyoshi@dmm.com>
2019-08-15 01:18:39 +0900

    :recycle: namespaceの修正

--- app/Models/Posts.php
+++ app/Models/Posts.php
@@ -4,6 +4,14 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * App\Models\Posts
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Posts newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Posts newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Posts query()
+ * @mixin \Eloquent
+ */
 class Posts extends Model
 {
     //
