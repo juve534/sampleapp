f494d2473cf9864b06bb7ceade22d75596619432
kimura-tsuyoshi <kimura-tsuyoshi@dmm.com>
2019-08-16 03:31:07 +0900

    :sparkles: リソースの作成

--- app/Http/Resources/PostsResource.php
+++ app/Http/Resources/PostsResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\Resource;
+use Illuminate\Support\Collection;
 
 class PostsResource extends Resource
 {
@@ -14,6 +15,13 @@ class PostsResource extends Resource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return [
+            'id' => $this->resource['id'],
+            'title' => $this->resource['title'],
+            'body' => $this->resource['body'],
+            'comments' => new CommentResourceCollection(
+                new Collection($this->resource['comments'])
+            ),
+        ];
     }
 }
