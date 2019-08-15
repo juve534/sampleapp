4973471ab89d66adcb48cb2a828d9bae2f90b6ae
kimura-tsuyoshi <kimura-tsuyoshi@dmm.com>
2019-08-16 03:30:22 +0900

    :rotating_light: 単一のデータ取得処理のテスト作成

--- tests/Feature/PostsTest.php
+++ tests/Feature/PostsTest.php
@@ -4,12 +4,14 @@ namespace Tests\Feature;
 
 use App\Models\Comments;
 use App\Models\Posts;
+use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\RefreshDatabase;
+use Mockery;
 
 class PostsTest extends TestCase
 {
-    use RefreshDatabase;
+    use RefreshDatabase, WithFaker;
 
     const BASE_URL = 'api/v1/posts';
 
@@ -28,10 +30,38 @@ class PostsTest extends TestCase
         );
     }
 
+    /**
+     * showメソッドの正常系
+     * @test
+     */
+    public function testShow200()
     {
+        // setup
+        $expected = factory(Posts::class)->create()->toArray();
+        unset($expected['created_at']);
+        unset($expected['updated_at']);
+
+        /** @var \Illuminate\Database\Eloquent\Collection $comments */
+        $comments = factory(Comments::class, 5)->create(
+            [
+                'posts_id' => $expected['id'],
+            ]
+        );
+        $expected['comments'] = $comments->map(function (Comments $item) {
+            $arr['id'] = $item->id;
+            $arr['commenter'] = $item->commenter;
+            $arr['body'] = $item->body;
+
+            return $arr;
+        })->toArray();
+
+        $response = $this->get(self::BASE_URL . '/' . $expected['id']);
         $response->assertStatus(200);
         $response->assertHeader('content-type', 'application/json');
         $response->assertExactJson(
+            [
+                'data' => $expected,
+            ]
         );
     }
 }
