6a39dbec7b989292cbb34640e9165e9bd32faad5
kimura-tsuyoshi <kimura-tsuyoshi@dmm.com>
2019-08-16 03:29:16 +0900

    :sparkles: 単一のデータ取得を作成

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Http\Resources\PostsResource;
 use App\Models\Posts;
+use App\Repositories\PostsRepositoryInterface as PostsRepository;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
@@ -14,6 +15,10 @@ class PostsController extends Controller
         $data = Posts::all();
         return JsonResponse::create($data);
     }
+
+    public function show(PostsRepository $postsRepository, Request $request)
+    {
+        $data = $postsRepository->findByIdAndComments((int) $request->id);
         return new PostsResource($data);
     }
 }
