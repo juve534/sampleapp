2c47aca9ad0ca018aadefc70277326746ea83c6c
juve534 <maxim_4141.rock@zoho.com>
2019-08-18 02:27:36 +0900

    :sparkles: レビューとタグを管理するModelを作成
    コマンドで作成して、IDEヘルパーを使ったまま.

---
+++ app/Models/Review.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\Review.
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Review newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Review newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\Review query()
+ * @mixin \Eloquent
+ */
+class Review extends Model
+{
+    //
+}
---
+++ app/Models/ReviewTag.php
@@ -0,0 +1,20 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * App\Models\ReviewTag.
+ *
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\ReviewTag newModelQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\ReviewTag newQuery()
+ * @method static \Illuminate\Database\Eloquent\Builder|\App\Models\ReviewTag query()
+ * @mixin \Eloquent
+ */
+class ReviewTag extends Model
+{
+    //
+}
