6be0473b31a2180646811cd4117ba0a32f913c31
juve534 <maxim_4141.rock@zoho.com>
2019-11-11 00:00:31 +0900

    Posts\IndexもResponderに置き換え (#26)
    * :rotating_light:  Indexのテストを適切な命名に修正
    
    Actionに切り直したため.
    
    * :recycle: IndexをResponderに置き換え

--- app/Http/Controllers/Posts/IndexAction.php
+++ app/Http/Controllers/Posts/IndexAction.php
@@ -5,15 +5,24 @@ declare(strict_types=1);
 namespace App\Http\Controllers\Posts;
 
 use App\Http\Controllers\Controller;
+use App\Http\Responders\Posts\PostsIndexJsonResponder;
 use App\Models\Posts;
 use Illuminate\Http\JsonResponse;
 
 final class IndexAction extends Controller
 {
-    public function __invoke()
-    {
-        $data = Posts::all();
+    /** @var PostsIndexJsonResponder */
+    private $responder;
 
-        return JsonResponse::create($data);
+    public function __construct(PostsIndexJsonResponder $responder)
+    {
+        $this->responder = $responder;
+    }
+
+    public function __invoke(): JsonResponse
+    {
+        $data = Posts::all()->toArray();
+
+        return $this->responder->respond($data);
     }
 }
