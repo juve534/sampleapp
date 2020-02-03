1ca659dbb51fbca9d8a018e79341cb5072825972
juve534 <maxim_4141.rock@zoho.com>
2020-02-04 00:35:09 +0900

    [ci skip] :books: Swaggerを生成
    PHP Docに記載し、Swaggerを生成.

--- app/Http/Controllers/Posts/ShowAction.php
+++ app/Http/Controllers/Posts/ShowAction.php
@@ -10,6 +10,46 @@ use App\Repositories\PostsRepositoryInterface as PostsRepository;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
 use Illuminate\Http\Request;
 
+/**
+ * @OA\Info(title="PostAPI", version="1.0")
+ */
+/**
+ * @OA\Schema(
+ *   schema="posts",
+ *   type="object",
+ *   description="参照用",
+ *   @OA\Property(
+ *     property="id",
+ *     type="integer",
+ *     description="ブログID"
+ *   ),
+ *   @OA\Property(
+ *     property="title",
+ *     type="string",
+ *     description="ブログタイトル"
+ *   ),
+ *   @OA\Property(
+ *     property="comments",
+ *     type="object",
+ *     description="コメント者一覧",
+ *     @OA\Property(
+ *       property="id",
+ *       type="integer",
+ *       description="コメントID"
+ *     ),
+ *     @OA\Property(
+ *       property="commeter",
+ *       type="string",
+ *       description="コメントをした人"
+ *     ),
+ *     @OA\Property(
+ *       property="body",
+ *       type="string",
+ *       description="コメント内容"
+ *     )
+ *   )
+ * )
+ */
 final class ShowAction extends Controller
 {
     /** @var PostsShowJsonResponder */
@@ -20,6 +60,28 @@ final class ShowAction extends Controller
         $this->responder = $responder;
     }
 
+    /**
+     * @OA\Post(
+     *   path="/api/v1/posts/{id}",
+     *   summary="ブログ記事の情報を取得する.",
+     *   @OA\Parameter(
+     *     name="id",
+     *     in="path",
+     *     required=true,
+     *     description="ブログID",
+     *     @OA\Schema(type="integer")
+     *   ),
+     *   @OA\Response(
+     *     response=200,
+     *     description="OK",
+     *     @OA\JsonContent(@OA\Property(property="data", ref="#/components/schemas/posts"))
+     *   ),
+     *   @OA\Response(
+     *     response=404,
+     *     description="Not Found"
+     *   )
+     * )
+     */
     public function __invoke(PostsRepository $postsRepository, Request $request)
     {
         try {
